e51b5e13f65842b0044e5b0de23367a3730a39bf
zizimoos_iMac <zizimoos@gmail.com>
2020-11-25 19:20:54 +0900

    ...

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,8 +1,9 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersOutput, GetOrdersInput } from './dtos/get-orders.dto';
 import { Order } from './entities/order.entity';
 import { OrdersService } from './orders.service';
 
@@ -19,5 +20,14 @@ export class OrdersResolver {
     createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.createOrder(customer, createOrderInput);
+  }
+
+  @Query(() => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return await this.ordersService.getOrder(user, getOdersInput);
   }
 }
