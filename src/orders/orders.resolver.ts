cd502e8c401656bcb0f61b0c0d8d0fa7c04842df
zizimoos_iMac <zizimoos@gmail.com>
2020-11-28 00:15:53 +0900

    ORDER SUBSCRIPTIONS done!

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -16,6 +16,7 @@ import { EditOrderInput, EditOrderOutput } from './dtos/edit-order.dto';
 import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { GetOrdersOutput, GetOrdersInput } from './dtos/get-orders.dto';
 import { OrderUpdateInput } from './dtos/order-updates.dto';
+import { TakeOrderInput, TakeOrderOutput } from './dtos/take-order.dto';
 import { Order } from './entities/order.entity';
 import { OrdersService } from './orders.service';
 
@@ -102,5 +103,14 @@ export class OrdersResolver {
   orderUpdates(@Args('input') orderUpdateInput: OrderUpdateInput) {
     console.log(orderUpdateInput);
     return this.pubSub.asyncIterator(NEW_ORDER_UPDATE);
+  }
+
+  @Mutation(() => TakeOrderOutput)
+  @Role(['Delivery'])
+  takeOrder(
+    @AuthUser() dirver: User,
+    @Args('input') takeOrderInput: TakeOrderInput,
+  ): Promise<TakeOrderOutput> {
+    return this.ordersService.takeOrder(dirver, takeOrderInput);
   }
 }
